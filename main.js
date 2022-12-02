134b1a3a1e306a9c29f096e8034bcbbf6d93d600
Areeb <82109141+ARE-coder@users.noreply.github.com>
2022-12-02 13:54:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var a = document.getElementById("song");
+var b = document.getElementById("video");
+function submit(){
+    document.getElementById("BDY").style.display = "block";
+    var a = document.getElementById("B").value; 
+    
+
+}
+function BD(){
+    var a = document.getElementById("B").value; 
+    alert("Happy Birthday To " + a + ". Here is my gift.") 
+    document.getElementById("Birth").style.display = "block";
+    document.getElementById("BDY").style.display = "none";
+    document.getElementById("hey").style.display = "none";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("and").style.display = "block"
+    document.getElementById("video").style.display = "block"
+    document.body.style.backgroundImage = "url('rick-astly-rick-rolled.gif')"
+    var a = document.getElementById("song");
+    a.play();
+    a.loop();
+   
+    
+}
